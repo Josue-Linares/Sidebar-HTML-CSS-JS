b68dc692049679e918acc091d1508983739c3581
josue-linares <alex.dev.fs503@gmail.com>
2023-09-29 18:50:56 -0600

    sidebar test and practice

---
+++ script.js
@@ -0,0 +1,15 @@
+var side = true;
+
+function toggleSidebar() {
+    if (side) {
+        console.log("opening sidebar");
+        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+        this.side = false;
+    } else {
+        console.log("closing sidebar");
+        document.getElementById("mySidebar").style.width = "85px";
+        document.getElementById("main").style.marginLeft = "85px";
+        this.side = true;
+    }
+}
